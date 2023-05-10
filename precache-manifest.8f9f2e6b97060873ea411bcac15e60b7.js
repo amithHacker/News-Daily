3d60d26c2b2b905ad06c525be553ca91255935c2
amithHacker <132680235+amithHacker@users.noreply.github.com>
2023-05-10 13:53:21 +0530

    Updates

---
+++ precache-manifest.8f9f2e6b97060873ea411bcac15e60b7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec857bd88b35a11a5cb6",
+    "url": "/NewsDaily/static/css/main.37ad4359.chunk.css"
+  },
+  {
+    "revision": "ec857bd88b35a11a5cb6",
+    "url": "/NewsDaily/static/js/main.ec857bd8.chunk.js"
+  },
+  {
+    "revision": "b9dd121dd254d67d1d56",
+    "url": "/NewsDaily/static/js/1.b9dd121d.chunk.js"
+  },
+  {
+    "revision": "3cb43352a300f7a336e7",
+    "url": "/NewsDaily/static/js/2.3cb43352.chunk.js"
+  },
+  {
+    "revision": "4cb488d0ff0c74116a8c",
+    "url": "/NewsDaily/static/js/runtime~main.4cb488d0.js"
+  },
+  {
+    "revision": "e201144f908cbb7743b737381db54636",
+    "url": "/NewsDaily/static/media/logo.e201144f.svg"
+  },
+  {
+    "revision": "03aaebf151af139bd05df53dc5b362c3",
+    "url": "/NewsDaily/static/media/aboutus.03aaebf1.jpg"
+  },
+  {
+    "revision": "95421bceb1ac9ea35e1619a62f0300e6",
+    "url": "/NewsDaily/static/media/wwr1.95421bce.png"
+  },
+  {
+    "revision": "6d3376be3c0233c0f348d3b27dd0da6c",
+    "url": "/NewsDaily/static/media/wwr2.6d3376be.png"
+  },
+  {
+    "revision": "2106461b23279116462531779eaf72c8",
+    "url": "/NewsDaily/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/NewsDaily/precache-manifest.8f9f2e6b97060873ea411bcac15e60b7.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/NewsDaily/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
